313f97826b04e7d3a317ff0a90f2edb1a0fb108c
Yash <yash.p@nimblechapps.in>
2026-03-23 10:51:55 +0530

    Added new project

---
+++ ExpoRouter_DrawerNavigation/app/_layout.tsx
@@ -0,0 +1,57 @@
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import  { Drawer } from 'expo-router/drawer';
+import  FontAwesome  from "@expo/vector-icons/FontAwesome";
+
+export default function RootLayout() {
+  return (
+    <GestureHandlerRootView>
+      <Drawer
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#ffffff",
+          },
+          headerTintColor: "#8b5cf6",
+          drawerStyle: {
+            backgroundColor: "#ffffff",
+          },
+          drawerActiveTintColor: "#8b5cf6",
+          drawerInactiveTintColor: "#64748b"
+        }}
+      >
+        <Drawer.Screen 
+          name = "index"
+          options={{
+            title: "Home title",
+            drawerLabel : "Home Label",
+            drawerIcon: ({color}) => (
+              <FontAwesome name="home" size={24} color={color} />
+            )
+          }}
+        />
+
+        <Drawer.Screen 
+          name = "dashboard"
+          options={{
+            title: "Dashboard title",
+            drawerLabel : "Dashboard Label",
+            drawerIcon: ({color}) => (
+              <FontAwesome name="dashboard" size={24} color={color} />
+            )
+          }}
+        />
+
+        <Drawer.Screen 
+          name = "settings"
+          options={{
+            title: "Settings title",
+            drawerLabel : "Settings Label",
+            drawerIcon: ({color}) => (
+              <FontAwesome name="cog" size={24} color={color} />
+            )
+          }}
+        />
+
+      </Drawer>
+    </GestureHandlerRootView>
+  );
+}
